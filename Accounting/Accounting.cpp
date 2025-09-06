9b49b5da719eec726a330a8a33de29f344ee871f
Ahmad ali <ahmadaliah099@gmail.com>
2025-09-06 16:00:54 +0300

    solve accounting

--- Accounting/Accounting.cpp
+++ Accounting/Accounting.cpp
@@ -1,49 +1,30 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
-
-int fact(int num)
-{
-	if (num == 0 || num == 1)
-		return 1;
-	else
-		return num * fact(num - 1);
-	
-}
 
 int main()
 {
 	int A; // income in year zero.
 	int B; // income after n years.
 	int n = 1; // number of years.
-	double x; //groth.
 
-	cin >> n;
+	// assuming -100 <= x <= 100
+	cin >> A >> B >> n;
 
-	if (n >= 1 && n <= 10)
+	bool found = false;
+
+	for (int x = -1000; x < 1000; x++)
 	{
-		double p = 1.0;
-		for (int i = 0; i < n; i++)
+		if (pow(x, n) == B / A)
 		{
-			p *= p;
+			cout << x;
+			found = true;
+			break;
 		}
-		x = p;
 	}
 
-	cin >> A >> B;
-
-	x = A / B;
-
-	fact(x);
-
-	if (x == (int)x)
-	{
-		cout << x;
-	}
-	else
-		cout << " error ";
-
-
-
-
+	if (!found)
+		cout << "no";
+	
 }
